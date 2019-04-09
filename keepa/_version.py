0c2354bd8c480c4561d6bcc447d0399918ff4e12
Alex Kaszynski <akascap@gmail.com>
2019-04-09 15:18:43 +0200

    version bump

--- keepa/_version.py
+++ keepa/_version.py
@@ -1,6 +1,6 @@
-"""
-Stores version number
+"""Version number for keepa
 """
 # major, minor, patch, -extra
-version_info = 0, 17, 1
+version_info = 0, 17, 2
+
 __version__ = '.'.join(map(str, version_info))
